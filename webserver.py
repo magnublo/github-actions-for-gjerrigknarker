1784a5e9303994be4d8dae8c637d82a31689152b
Magnus <magnus.longva@bouvet.no>
2021-04-14 14:20:11 +0200

    increased iterations. added text representation of nr of iterations

--- webserver.py
+++ webserver.py
@@ -5,14 +5,17 @@ import socket
 from http import HTTPStatus
 import os
 import psutil
-
+import datetime
+import hashlib
+from num2words import num2words
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
         current_time=str(datetime.datetime.now())
-        iterations=5
+        iterations=5000000
+        iterations_text=num2words(iterations)
         hash_result = current_time
         for i in range(iterations):
             hash_result = hashlib.sha512(hash_result.encode('utf-8')).hexdigest()
@@ -20,12 +23,12 @@ class Handler(http.server.SimpleHTTPRequestHandler):
 This backend is currently using {psutil.cpu_percent()}% of CPU and {psutil.virtual_memory().percent}% of RAM.
 
 Here, I have made an expensive computation for you. The current date and time is {current_time} (or so it was, right before I started computing hashes).
-If we hash this {iterations} times with SHA512, we get {hash_result}"""
+If we hash this {iterations} ({iterations_text}) times with SHA512, we get {hash_result}"""
         response_bytes = response.encode('utf-8')
         self.wfile.write(response_bytes)
 
 
-httpd = socketserver.TCPServer(('0.0.0.0', 8000), Handler)
+httpd = socketserver.TCPServer(('0.0.0.0', 8001), Handler)
 httpd.serve_forever()
 
 
