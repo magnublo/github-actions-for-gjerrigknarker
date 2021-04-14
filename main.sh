b0794a426c119b3e46842ff0cdc33f9d91a44b65
Magnus <magnus.longva@bouvet.no>
2021-04-14 12:41:36 +0200

    moved some logic to main.sh

---
+++ main.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+(autossh -N -M 0 -o ExitOnForwardFailure=yes -o "ServerAliveInterval 30" -o "ServerAliveCountMax 3" -R $((NODE_NR+10000)):127.0.0.1:8000 -i /tmp/id_rsa gh_actions_backend@ondehensikter.no) &
+$GITHUB_WORKSPACE/webserver.py &
+wait -n
