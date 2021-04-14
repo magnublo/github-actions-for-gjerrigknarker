f399d0a19cd85f216c43a6157fcedb83c4240c2e
Magnus <magnus.longva@bouvet.no>
2021-04-14 13:57:49 +0200

    made expensive computation

--- webserver.py
+++ webserver.py
@@ -4,13 +4,23 @@ import socketserver
 import socket
 from http import HTTPStatus
 import os
+import psutil
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        response = f"This message was sent from {socket.gethostname()} with actions run id {os.getenv('GITHUB_RUN_ID')}"
+        current_time=str(datetime.datetime.now())
+        iterations=10000
+        hash_result = current_time
+        for i in range(iterations):
+            hash_result = hashlib.sha512(hash_result.encode('utf-8')).hexdigest()
+        response = f"""This message was sent from {socket.gethostname()} with actions run id {os.getenv('GITHUB_RUN_ID')}
+This backend is currently using {psutil.cpu_percent()}% of CPU and {psutil.virtual_memory().percent}% of RAM.
+
+Here, I have made an expensive computation for you. The current date and time is {current_time} (or so it was, right before I started computing hashes).
+If we hash this {iterations} times with SHA512, we get {hash_result}"""
         response_bytes = response.encode('utf-8')
         self.wfile.write(response_bytes)
 
