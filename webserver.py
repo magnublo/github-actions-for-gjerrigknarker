9ba0b0d3d418e100980acb48a6359165e71d0140
Magnus <magnus.longva@bouvet.no>
2021-04-15 10:09:59 +0200

    refactoring and using html instead of plaintext

--- webserver.py
+++ webserver.py
@@ -1,52 +1,56 @@
 #!/usr/bin/python3
-import http.server
-import socketserver
-import socket
-from http import HTTPStatus
-import os
-import psutil
 import datetime
 import hashlib
+import http.server
+import os
+import socket
+import socketserver
+from http import HTTPStatus
+
+import psutil
 from num2words import num2words
+
+from settings import HASH_ITERATIONS
+
+LISTENING_PORT = os.getenv('PYTHON_PORT', 8000)
+
+
+def get_hex_red(cpu_percent):
+    return hex(min(int((cpu_percent/100)*512), 255))[-2:]
+
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        current_time=str(datetime.datetime.now())
-        iterations=5000000
-        iterations_text=num2words(iterations)
+        current_time = str(datetime.datetime.now())
+        iterations_text = num2words(HASH_ITERATIONS)
         hash_result = current_time
-        for i in range(iterations):
+        for i in range(HASH_ITERATIONS):
             hash_result = hashlib.sha512(hash_result.encode('utf-8')).hexdigest()
-        response = f"""This message was sent from a machine
-with name
+        cpu_percent = psutil.cpu_percent()
+        memory_percent = psutil.virtual_memory().percent
+        cpu_hex_red = get_hex_red(cpu_percent)
+        memory_hex_red = get_hex_red(memory_percent)
+        response = f"""<html><body>This message was sent from a machine
+with name<br>
 {socket.gethostname()}
-
-listening on load balancer port
+<br><br>
+listening on load balancer port<br>
 {os.getenv('lowest_available_port')}
-
-with actions run id
+<br><br>
+with actions run id<br>
 {os.getenv('GITHUB_RUN_ID')}
-
-This backend is currently using {psutil.cpu_percent()}% of CPU and {psutil.virtual_memory().percent}% of RAM.
-
+<br><br>
+This backend is currently using <span style="color:#{cpu_hex_red}0000;">{cpu_percent}%</span> of CPU and <span style="color:#{memory_hex_red}0000;">{memory_percent}%</span> of RAM.
+<br>
 I have made an expensive computation for you. The current date and time is {current_time} (or so it was, right before I started computing hashes).
-If we hash this {iterations} ({iterations_text}) times with SHA512, we get {hash_result}"""
+If we hash this {HASH_ITERATIONS} ({iterations_text}) times with SHA512, we get 
+<br><br>
+{hash_result}</html></body>"""
         response_bytes = response.encode('utf-8')
         self.wfile.write(response_bytes)
 
 
-httpd = socketserver.TCPServer(('0.0.0.0', 8000), Handler)
+httpd = socketserver.TCPServer(('0.0.0.0', LISTENING_PORT), Handler)
 httpd.serve_forever()
-
-
-# python3 server.py
-# 127.0.0.1 - - [11/Apr/2017 11:36:49] "GET / HTTP/1.1" 200 -
-# http :8000
-'''
-HTTP/1.0 200 OK
-Date: Tue, 11 Apr 2017 15:36:49 GMT
-Server: SimpleHTTP/0.6 Python/3.5.2
-Hello world
-'''
